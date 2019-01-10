4dda1e66e11fb998c449b9161a58870517e3075d
you <you>
2019-01-11 04:35:00 +0800

    v1.2

--- ruby/lib/ruby/site_ruby/2.5.0/ruby_installer/runtime/package_version.rb
+++
@@ -1,6 +0,0 @@
-module RubyInstaller
-module Runtime
-PACKAGE_VERSION = "2.5.3-1"
-GIT_COMMIT = "16e7aa4"
-end
-end
---
+++ ruby/lib/ruby/site_ruby/2.6.0/ruby_installer/runtime/package_version.rb
@@ -0,0 +1,6 @@
+module RubyInstaller
+module Runtime
+PACKAGE_VERSION = "2.6.0-1"
+GIT_COMMIT = "7688b64"
+end
+end
